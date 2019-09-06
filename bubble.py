98af3dbac7e3346c758176d507514768b96f2bcd
Aminta-Rebecca <53899468+Aminta-Rebecca@users.noreply.github.com>
2019-09-06 20:29:54 +0530

    Add files via upload

---
+++ bubble.py
@@ -0,0 +1,16 @@
+a=[2,54,12,7,8,3,21,56,789,231,45,23,76,23]
+#find 3
+
+def bubble(a):
+    for i in range (0,len(a)-1):
+        for j in range (0,len(a)-1-i):  
+            if(a[j]>a[j+1]):
+                temp=a[j]
+                a[j]=a[j+1]
+                a[j+1]=temp
+    return a   
+  
+ 
+bubble(a)
+print("Sorted array")    
+print(a)
